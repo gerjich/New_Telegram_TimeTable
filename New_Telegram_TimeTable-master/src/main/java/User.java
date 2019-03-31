47ffca302f25b2521a8fa3f45574c4b7d6666a14
MarinaKonovalova <33580319+MarinaKonovalova@users.noreply.github.com>
2019-03-31 23:21:32 +0500

    Update User.java

--- New_Telegram_TimeTable-master/src/main/java/User.java
+++ New_Telegram_TimeTable-master/src/main/java/User.java
@@ -1,10 +1,34 @@
-public class User {
-    public Long id = null;
-    public String group = null;
-    public String day = null;
+public class User  {
+    public static Long ID = null;
+    public static String Group = null;
+    public static String Day = null;
+    public static String Name = null;
+    public static Boolean Teacher = null;
 
-    public void setId(Long id){
-        this.id = id;
+    public static void setID(Long id){
+        ID = id;
     }
 
+    public static void changeGroup(String group){
+        Group = group;
+    }
+
+    public static void changeDay(String day){
+        Day = day;
+    }
+
+    public static void changeName(String name){
+        Name = name;
+    }
+
+    public static Boolean getRights(String password){
+        if (password.equals("12345")){
+            Teacher = true;
+            return true;
+        }
+        else {
+            Teacher = false;
+            return false;
+        }
+    }
 }
